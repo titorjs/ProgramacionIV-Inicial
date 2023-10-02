f1836a9d0bda2b5851ebe2aa8b71c46e6d9a589a
titorjs <titoj740@gmail.com>
2023-10-02 08:46:16 -0500

    Agregar y eliminar producto

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProgramacionIV.Models;
 using ProgramacionIV.NewFolder;
 
 
@@ -9,33 +10,48 @@ namespace ProgramacionIV.Controllers
     public class ProductoController : Controller
     {
         // GET: ProductoController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View(Utils.ListaProductos);
         }
 
         // GET: ProductoController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
         // GET: ProductoController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Producto p)
+        {
+            int cant = Utils.ListaProductos.Count();
+            p.IdProducto = cant + 1;
+            Utils.ListaProductos.Add(p);
             return View();
         }
 
         // GET: ProductoController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return RedirectToAction("Index");
         }
 
         // GET: ProductoController/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int IdProducto)
         {
-            return View();
+            Producto p = Utils.ListaProductos.Find(x => x.IdProducto == IdProducto);
+            if(p != null)
+            {
+                Utils.ListaProductos.Remove(p);
+            }
+            
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,10 +1,14 @@
-﻿namespace ProgramacionIV.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionIV.Models
 {
     public class Producto
     {
         public int IdProducto { get; set; }
+        [Required(ErrorMessage = "Pon el nombre de a verga")]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Pon la cantidad mmvrg")]
         public int Cantidad { get; set; }
     }
 }
