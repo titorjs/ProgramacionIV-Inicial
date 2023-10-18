9102307f3a666a5f238a1eac504ebb88b0b0767d
titorjs <titoj740@gmail.com>
2023-10-17 23:22:46 -0500

    Conexión con la API

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,30 +1,55 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ProgramacionIV.Models;
 using ProgramacionIV.NewFolder;
-
-
+using System.Text.Json.Serialization;
 
 namespace ProgramacionIV.Controllers
 {
     public class ProductoController : Controller
     {
+        Uri baseAdd = new Uri("https://localhost:7106/api/Producto");
+        private readonly HttpClient _client;
+
+        public ProductoController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAdd;
+        }
+
         // GET: ProductoController
         public IActionResult Index()
         {
-            return View(Utils.ListaProductos);
+            List<Producto> products = new List<Producto>();
+
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<List<Producto>>(data);
+            }
+
+            return View(products);
         }
 
         // GET: ProductoController/Details/5
         public IActionResult Details(int IdProducto)
         {
-			Producto p = Utils.ListaProductos.Find(x => x.IdProducto == IdProducto);
-			if (p != null)
+            Producto p;
+
+			HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/" + IdProducto).Result;
+
+			if (response.IsSuccessStatusCode)
 			{
-				return View(p);
+				string data = response.Content.ReadAsStringAsync().Result;
+				p = JsonConvert.DeserializeObject<Producto>(data);
+
+                return View(p);
 			}
 
-            return RedirectToAction("Index");
+			return RedirectToAction("Index");
 		}
 
         // GET: ProductoController/Create
@@ -36,47 +61,40 @@ namespace ProgramacionIV.Controllers
         [HttpPost]
         public IActionResult Create(Producto p)
         {
-            int cant = Utils.ListaProductos.Count();
-            p.IdProducto = cant + 1;
-            Utils.ListaProductos.Add(p);
-            return View();
+            HttpResponseMessage response = _client.PostAsJsonAsync(_client.BaseAddress, p).Result;
+			return RedirectToAction("Index");
         }
 
         // GET: ProductoController/Edit/5
         public ActionResult Edit(int IdProducto)
         {
-            Producto p = Utils.ListaProductos.Find(x => x.IdProducto == IdProducto);
-            if (p != null)
-            {
-                return View(p);
-            }
-            return RedirectToAction("Index");
+            Producto p;
+			HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/" + IdProducto).Result;
+
+			if (response.IsSuccessStatusCode)
+			{
+				string data = response.Content.ReadAsStringAsync().Result;
+				p = JsonConvert.DeserializeObject<Producto>(data);
+
+				return View(p);
+			}
+			return RedirectToAction("Index");
         }
 
         [HttpPost]
         public ActionResult Edit(Producto Producto)
         {
-            Producto p = Utils.ListaProductos.Find(x => x.IdProducto == Producto.IdProducto);
-            
-            if (p != null)
-            {
-                p.Nombre = Producto.Nombre;
-                p.Cantidad = Producto.Cantidad;
-                p.Descripcion = Producto.Descripcion;
-            }
+			HttpResponseMessage response = _client.PutAsJsonAsync(_client.BaseAddress + "/" + Producto.IdProducto, Producto).Result;
+
             return RedirectToAction("Index");
         }
 
         // GET: ProductoController/Delete/5
         public ActionResult Delete(int IdProducto)
         {
-            Producto p = Utils.ListaProductos.Find(x => x.IdProducto == IdProducto);
-            if(p != null)
-            {
-                Utils.ListaProductos.Remove(p);
-            }
-            
-            return RedirectToAction("Index");
+			HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "/" + IdProducto).Result;
+
+			return RedirectToAction("Index");
         }
     }
 }
